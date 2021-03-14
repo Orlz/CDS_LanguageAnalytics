408357534499c5a634bdce68cfc0dfc998f48504
Orlz <52678852+Orlz@users.noreply.github.com>
2021-03-14 18:47:19 +0100

    Create create_lang_venv.sh

---
+++ Assignments/Assignment04/create_lang_venv.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+VENVNAME=VE_Networks
+
+python3 -m venv $VENVNAME
+source $VENVNAME/bin/activate
+pip install --upgrade pip
+
+pip install ipython
+pip install jupyter
+pip install pandas
+
+#python -m ipykernel install --user --name=$VENVNAME
+
+test -f requirements.txt && pip install -r requirements.txt
+
+deactivate
+echo "build $VENVNAME"
